536be91c0ea8a951d431ce350dbe01ad0e7001cc
amolina20 <dbgthd413@gmail.com>
2025-05-03 17:38:06 -0400

    Ver Productos Añadidos

--- src/app/ver_inventario/page.jsx
+++ src/app/ver_inventario/page.jsx
@@ -1,70 +1,111 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Datos from '@/datos_inventario';
 
 export default function VerInv() {
   const router = useRouter();
+  const [inventario, setInventario] = useState([]);
+  const [productosBusqueda, setProductosBusqueda] = useState([]);
   const [idBusqueda, setIdBusqueda] = useState("");
-  const [categoriaBusqueda, setCategoriaBusqueda] = useState("");
-  const [inventario, setInventario] = useState(Datos);
+  const [categoriaBusqueda, setCategoriaBusqueda] = useState("")
 
   useEffect(() => {
     const sesion = localStorage.getItem('logeado');
     if (sesion !== 'true') {
       router.push('/login');
-    } 
+    } else {
+      const inventarioGuardado = JSON.parse(localStorage.getItem('inventario')) || [];
+      const inventarioActual= inventarioGuardado.map(item => ({
+        ...item,
+        stock: Number(item.stock),
+      }));
+      setInventario(inventarioActual);  
+      setProductosBusqueda(inventarioActual); 
+    }
   }, [router]);
-
-  const busquedaInventario = (e) => {
-    const idBusqueda = e.target.value;
-    setIdBusqueda(idBusqueda);
+  
+  
+  const aplicarFiltros = (data) => {
+    if (idBusqueda !== "") {
+      return data.filter(item => item.id === parseInt(idBusqueda));
+    }
+    if (categoriaBusqueda !== "") {
+      return data.filter(item => item.categoria === categoriaBusqueda);
+    }
+    return data;
   };
-
-  const busquedaInventarioCategoria = (e) => {
-    const catBusqueda = e.target.value;
-    setCategoriaBusqueda(catBusqueda);
-  };
-
+  
   const aumentarStock = (id) => {
-    setInventario(inventario.map(item => {
-      if (item.id === id) {
-        return { ...item, stock: item.stock + 1 };
-      }
-      return item;
-    }));
-  };
+  const inventarioActualizado = inventario.map(item => {
+    if (item.id === id) {
+      return { ...item, stock: item.stock + 1 };
+    }
+    return item;
+  });
+
+  const productosFiltrados = aplicarFiltros(inventarioActualizado);
+  setProductosBusqueda(productosFiltrados);
+  setInventario(inventarioActualizado);
+  localStorage.setItem('inventario', JSON.stringify(inventarioActualizado));
+};
 
   const disminuirStock = (id) => {
-    setInventario(inventario.map(item => {
+    const inventarioActualizado = inventario.map(item => {
       if (item.id === id && item.stock > 0) {
         return { ...item, stock: item.stock - 1 };
       }
       return item;
-    }));
-  };
+    });
 
+  const productosFiltrados = aplicarFiltros(inventarioActualizado);
+  setProductosBusqueda(productosFiltrados);
+  setInventario(inventarioActualizado);
+  localStorage.setItem('inventario', JSON.stringify(inventarioActualizado));
+};
+
+
+  const busquedaInventario = (e) => {
+    const idBusqueda = e.target.value.trim();
+    setIdBusqueda(idBusqueda);
+    if (idBusqueda == ""){
+      setProductosBusqueda(inventario);
+    } else {
+      const productosFiltrados = inventario.filter(item => item.id === parseInt(idBusqueda));
+      setProductosBusqueda(productosFiltrados);
+    }
+  }
+
+  const busquedaInventarioCategoria = (e) => {
+    const catBusqueda = e.target.value.trim();
+    setCategoriaBusqueda(catBusqueda);
+    if (catBusqueda == ""){
+      setProductosBusqueda(inventario);
+    } else {
+      const productosFiltrados = inventario.filter(item => item.categoria === catBusqueda);
+      setProductosBusqueda(productosFiltrados);
+    }
+  }
   return (
     <div className="flex flex-col min-h-screen text-black px-6 py-8">
       <h1 className="text-center text-4xl font-bold mb-12 text-blue-950">
         Lista de Inventariado   
       </h1>
       <div className="mb-6">
-        <label htmlFor="idProducto" className="block text-lg mb-2">
-          Buscar Producto por ID
-        </label>
-        <input
-          type="number"
-          id="idProducto"
-          name="idProducto"
-          placeholder="Ingrese ID Producto"
-          className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700"
-          onChange={busquedaInventario}
-          disabled={categoriaBusqueda !== ""}
-        />
-      </div>
+          <label htmlFor="idProducto" className="block text-lg mb-2">
+            Buscar Producto por ID
+          </label>
+          <input
+            type="number"
+            id="idProducto"
+            name="idProducto"
+            placeholder="Ingrese ID Producto"
+            className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700"
+            onChange={busquedaInventario}
+            disabled={categoriaBusqueda !== ""}
+          />
+        </div>
       <div>
-        <div className="mb-6">
+      <div className="mb-6">
           <label htmlFor="idProducto" className="block text-lg mb-2">
             Buscar Por Categoria
           </label>
@@ -78,7 +119,7 @@ export default function VerInv() {
             disabled={idBusqueda !== ""}
           />
         </div>
-
+        
         <table className="min-w-full bg-gray-400">
           <thead>
             <tr className="bg-gray-600 text-white">
@@ -91,7 +132,7 @@ export default function VerInv() {
             </tr>
           </thead>
           <tbody>
-            {inventario.map((item) => (
+            {productosBusqueda.map((item) => (
               <tr key={item.id} className="text-white">
                 <td className="border px-3 py-2">{item.id}</td>
                 <td className="border px-3 py-2">{item.nombre}</td>
@@ -100,7 +141,7 @@ export default function VerInv() {
                 <td className="border px-3 py-2">{item.categoria}</td>
                 <td className="border px-3 py-2">
                   <button className="bg-green-400 px-2 rounded mr-1 hover:bg-green-700" onClick={() => aumentarStock(item.id)}>+</button>
-                  <button className="bg-yellow-600 px-2 rounded ml-1 hover:bg-red-700" onClick={() => disminuirStock(item.id)}>−</button>
+                  <button className="bg-yellow-600 px-2 rounded ml-1 hover:bg-red-700" onClick={() => disminuirStock(item.id)} >−</button>
                 </td>
               </tr>
             ))}           
