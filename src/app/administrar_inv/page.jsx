cf74b8a41261e37f5d3ba53155e5ee3a2e1eafa6
amolina20 <dbgthd413@gmail.com>
2025-05-03 17:36:39 -0400

    Añadir Productos a Inventario

--- src/app/administrar_inv/page.jsx
+++ src/app/administrar_inv/page.jsx
@@ -1,12 +1,15 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function AdminInv() {
-  const router = useRouter();  
-  const [mostrarMensaje, setMostrarMensaje] = useState(false);
-  
+  const router = useRouter();
+
+  const [nombre, setNombre] = useState('');
+  const [precio, setPrecio] = useState('');
+  const [stock, setStock] = useState('');
+  const [categoria, setCategoria] = useState('')
+
   useEffect(() => {
     const sesion = localStorage.getItem('logeado');
     const rol = localStorage.getItem('rol')
@@ -21,13 +24,22 @@ export default function AdminInv() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMostrarMensaje(true);
+
+    const nuevoProducto = {id: Date.now(), nombre, precio, stock, categoria};
+    const inventarioGuardado = JSON.parse(localStorage.getItem('inventario')) || [];
+    inventarioGuardado.push(nuevoProducto);
+    localStorage.setItem('inventario', JSON.stringify(inventarioGuardado));
+
+    setNombre('');
+    setPrecio('');
+    setStock('');
+    setCategoria('');
   };
 
   return (
     <div className="flex flex-col min-h-screen text-black px-6 py-8">
       <h1 className="text-center text-4xl font-bold mb-12 text-blue-950">
-        Administrar Inventario
+        Administrar Inventario 
       </h1>
 
       <h2 className="text-2xl font-semibold mb-4">Agregar Producto</h2>
@@ -41,6 +53,8 @@ export default function AdminInv() {
             id="nombreProducto"
             name="nombreProducto"
             placeholder="Ingrese Producto"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
             required
             className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700"
           />
@@ -55,11 +69,12 @@ export default function AdminInv() {
             id="precioProducto"
             name="precioProducto"
             placeholder="Ingrese Precio"
+            value={precio}
+            onChange={(e) => setPrecio(e.target.value)}
             required
             className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700"
           />
         </div>
-
         <div className="mb-6">
           <label htmlFor="stockProducto" className="block text-lg mb-2">
             Stock Inicial
@@ -69,11 +84,12 @@ export default function AdminInv() {
             id="stockProducto"
             name="stockProducto"
             placeholder="Ingrese Stock Inicial"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
             required
             className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700"
           />
         </div>
-
         <div className="mb-6">
           <label htmlFor="categoriaProducto" className="block text-lg mb-2">
             Categoria
@@ -83,28 +99,16 @@ export default function AdminInv() {
             id="categoriaProducto"
             name="categoriaProducto"
             placeholder="Ingrese Categoria del Producto"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
             required
             className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-700"
           />
         </div>
-
         <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-fit mb-4">
           Agregar Producto
         </button>
       </form>
-      {mostrarMensaje && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <p className="text-xl font-bold">¡Agregaste un producto exitosamente!</p>
-            <button
-              onClick={() => setMostrarMensaje(false)}
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-4 hover:bg-blue-700"
-            >
-              Cerrar
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
